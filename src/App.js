5c99453f0f887aa9338ca3a51b5b26d36d1e444f
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2021-05-14 21:08:07 -0400

    Directory Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'; 
+import Directory from './components/DirectoryComponent';
 import './App.css';
 
 class App extends Component {
@@ -12,6 +12,7 @@ class App extends Component {
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
+              <Directory />
           </div>
       );
   }
